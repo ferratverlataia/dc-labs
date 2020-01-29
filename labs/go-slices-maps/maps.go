c2ac71184e866866efc66d0f80cf01b6f1bf6a37
ivan ferrat <ivanferrat@MacBook-Pro-de-ivan.local>
2020-01-29 16:08:48 -0600

    added two go files
    Signed-off-by: ivan ferrat <ivanferrat@MacBook-Pro-de-ivan.local>

--- labs/go-slices-maps/maps.go
+++ labs/go-slices-maps/maps.go
@@ -1,4 +1,20 @@
 package main
 
+import (
+	"strings"
+
+	"golang.org/x/tour/wc"
+)
+
+func WordCount(s string) map[string]int {
+	a := make(map[string]int)
+	b := strings.Fields(s)
+	for i := 0; i < len(b); i++ {
+		a[b[i]]++
+	}
+	return a
+}
+
 func main() {
+	wc.Test(WordCount)
 }
--- labs/go-slices-maps/slices.go
+++ labs/go-slices-maps/slices.go
@@ -1,4 +1,25 @@
 package main
 
+import "golang.org/x/tour/pic"
+
+func Pic(dx, dy int) [][]uint8 {
+
+	slice := make([][]uint8, dx)
+
+	for z := 0; z < dx; z++ {
+		slice[z] = make([]uint8, dy)
+	}
+
+	for i := 0; i < dx; i++ {
+		for z := 0; z < dy; z++ {
+
+			slice[i][z] = uint8((i * z) / 2)
+		}
+	}
+
+	return slice
+}
+
 func main() {
+	pic.Show(Pic)
 }

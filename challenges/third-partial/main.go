56eb22785253262510a8a98c83a1406a4d1b15e9
ferratverlataia <ivanferrat65@gmail.com>
2020-05-07 09:03:03 -0500

    changes

--- challenges/third-partial/main.go
+++ challenges/third-partial/main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
-	 "github.com/ferratverlataia/dc-labs/challenges/third-partial/api"
+	"fmt"
 	"github.com/ferratverlataia/dc-labs/challenges/third-partial/controller"
 	"github.com/ferratverlataia/dc-labs/challenges/third-partial/scheduler"
+	"github.com/ferratverlataia/dc-labs/challenges/third-partial/api"
 	"log"
-
+	"math/rand"
+	"time"
 )
 
 func main() {
@@ -18,7 +20,13 @@ func main() {
 	jobs := make(chan scheduler.Job)
 	go scheduler.Start(jobs)
 	// Send sample jobs
+	sampleJob := scheduler.Job{Address: "localhost:50051", RPCName: "hello"}
 
+	for {
+		sampleJob.RPCName = fmt.Sprintf("hello-%v", rand.Intn(10000))
+		jobs <- sampleJob
+		time.Sleep(time.Second * 5)
+	}
 
 	// API
 	// Here's where your API setup will be
